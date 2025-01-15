4dff2688f57326b4a4e1dcd31690cf62e1529b94
Spreadborough Stephen - Senior Data Scientist <S.Spreadborough@nottshc.nhs.uk>
2025-01-15 21:53:21 +0000

    testing of functions

--- tests/testthat/test-app_R6.R
+++ tests/testthat/test-app_R6.R
@@ -3,14 +3,24 @@
 # Test app_r6  app_data class methods
 #------------------------------------------------------------------------------#
 
-# note: have tests also
-
-
 # create dash_data object
 dash_data <- app_data$new()
 
+dash_data$date_range <- dash_data$data_date_range
 
-# test filter_ref_date ---------------------------------------------------------
+# test metric_meta ------------------------------------------------------------#
+
+testthat::test_that("app_data - metric_meta", {
+
+  expect_equal(
+    sort(colnames(dash_data$metric_meta)),
+    sort(c("metric_id", "metric_name", "metric_detail", "value_type"))
+  )
+
+
+})
+
+# test filter_ref_date --------------------------------------------------------#
 
 testthat::test_that("app_data - filter_ref_date", {
 
@@ -52,8 +62,6 @@ testthat::test_that("app_data - filter_ref_date", {
 
   expect_equal(date_2, all_periods)
 
-  dash_data$date_period_options
-
   # current week
   current_week <- dash_data$filter_ref_date(
     start = start_date,
@@ -61,7 +69,7 @@ testthat::test_that("app_data - filter_ref_date", {
     period = "0_week_current"
   )
 
-  expect_true(max(current_week$date) == max(as.Date(dash_data$stats19$datetime)))
+  expect_true(max(current_week$date) == max(as.Date(dash_data$stats19$date)))
   expect_true(weekdays(min(current_week$date)) == "Monday")
 
   # current month
@@ -71,7 +79,7 @@ testthat::test_that("app_data - filter_ref_date", {
     period = "0_month_current"
   )
 
-  expect_true(max(current_month$date) == max(as.Date(dash_data$stats19$datetime)))
+  expect_true(max(current_month$date) == max(as.Date(dash_data$stats19$date)))
   expect_true(day(min(current_month$date)) == 1)
   expect_true(day(max(current_month$date) + 1) == 1)
 
@@ -82,7 +90,7 @@ testthat::test_that("app_data - filter_ref_date", {
     period = "0_f_quarter_current"
   )
 
-  expect_true(max(current_f_quarter$date) == max(as.Date(dash_data$stats19$datetime)))
+  expect_true(max(current_f_quarter$date) == max(as.Date(dash_data$stats19$date)))
   expect_true(length(unique(current_f_quarter$month_year)) == 3)
   expect_true(length(unique(current_f_quarter$month)) == 3)
   expect_true(length(unique(current_f_quarter$f_year)) == 1)
@@ -96,7 +104,7 @@ testthat::test_that("app_data - filter_ref_date", {
     period = "11_month_current"
   )
 
-  expect_true(max(current_12_months$date) == max(as.Date(dash_data$stats19$datetime)))
+  expect_true(max(current_12_months$date) == max(as.Date(dash_data$stats19$date)))
   expect_true(length(unique(current_12_months$month_year)) == 12)
   expect_true(length(unique(current_12_months$month)) == 12)
   expect_true(current_12_months |>
@@ -111,7 +119,7 @@ testthat::test_that("app_data - filter_ref_date", {
     period = "0_f_year_current"
   )
 
-  expect_true(max(current_f_year$date) == max(as.Date(dash_data$stats19$datetime)))
+  expect_true(max(current_f_year$date) == max(as.Date(dash_data$stats19$date)))
   expect_true(length(unique(current_f_year$f_year)) == 1)
   expect_true(months(min(current_f_year$date)) == "April")
 
@@ -122,7 +130,7 @@ testthat::test_that("app_data - filter_ref_date", {
     period = "23_month_current"
   )
 
-  expect_true(max(current_24_months$date) == max(as.Date(dash_data$stats19$datetime)))
+  expect_true(max(current_24_months$date) == max(as.Date(dash_data$stats19$date)))
   expect_true(length(unique(current_24_months$month_year)) == 24)
   expect_true(length(unique(current_24_months$month)) == 12)
   expect_true(current_24_months |>
@@ -141,10 +149,229 @@ testthat::test_that("app_data - filter_ref_date", {
   expect_true(weekdays(min(last_week$date)) == "Monday")
   expect_true((max(last_week$date) + 1) == min(current_week$date))
 
-  # last financil quarter
+  # last financial quarter
+  last_f_quarter<- dash_data$filter_ref_date(
+    start = start_date,
+    end = end_date,
+    period = "1_f_quarter"
+  )
+
+  expect_true((max(last_f_quarter$date) + 1) == min(current_f_quarter$date))
+
+  # Last 12 months
+  last_12_months <- dash_data$filter_ref_date(
+    start = start_date,
+    end = end_date,
+    period = "12_month"
+  )
+
+  expect_false(max(last_12_months$date) == max(as.Date(dash_data$stats19$date)))
+  expect_true(length(unique(last_12_months$month_year)) == 12)
+  expect_true(length(unique(last_12_months$month)) == 12)
+  expect_true(last_12_months |>
+                select(month, calender_year) |>
+                unique() |>
+                nrow() == 12)
+
+  # last 24 months
+  last_24_months <- dash_data$filter_ref_date(
+    start = start_date,
+    end = end_date,
+    period = "23_month"
+  )
+
+  expect_false(max(last_24_months$date) == max(as.Date(dash_data$stats19$date)))
+  expect_true(length(unique(last_24_months$month)) == 12)
+})
+
+# test app_metrics ------------------------------------------------------------#
+
+testthat::test_that("app_data - app_metrics", {
+
+  # note: test variation of grouping variables from explore plot options,
+  # avoid selecting at random as need meaningful comparison on how long they
+  # take with shiny.benchmark
+
+  mets <- dash_data$metric_meta
+
+  # metric test 1 - first metric by date --------------------------------------#
+
+  x_axis <- "date"
+
+  test_1 <- dash_data$app_metrics(
+    metric_ids = mets$metric_id[1],
+    {{ sym(x_axis) }}
+    )
+
+  expect_true(
+    test_1$details$metric_id == mets$metric_id[1]
+    )
+
+  expect_true(
+    test_1$details$metric_name ==
+      mets$metric_name[mets$metric_id == mets$metric_id[1]]
+  )
+
+  expect_true(
+    x_axis %in% colnames(test_1$data)
+  )
+
+  expect_true(
+    max(test_1$data$date) == max(dash_data$stats19$date)
+  )
+
+  # metric test 2 - first metric by week start & grouping ---------------------#
+
+  x_axis <- "week_start"
+  group <- dash_data$explore_group_facet[3]
+
+  test_2 <- dash_data$app_metrics(
+    metric_ids = mets$metric_id[1],
+    {{ sym(x_axis) }},
+    {{ sym(group) }}
+  )
+
+  expect_true(
+    test_2$details$metric_id == mets$metric_id[1]
+  )
+
+  expect_true(
+    test_2$details$metric_name ==
+      mets$metric_name[mets$metric_id == mets$metric_id[1]]
+  )
+
+  expect_true(
+    x_axis %in% colnames(test_2$data)
+  )
+
+  expect_true(
+    group %in% colnames(test_2$data)
+  )
+
+  # metric test 3 - first metric by rolling 3 months & group & facet ----------#
+  x_axis <- "roll_3month"
+  group <- dash_data$explore_group_facet[3]
+  facet <- dash_data$explore_group_facet[4]
+
+  test_3 <- dash_data$app_metrics(
+    metric_ids = mets$metric_id[1],
+    {{ sym(x_axis) }},
+    {{ sym(group) }},
+    {{ sym(facet) }}
+  )
+
+  expect_true(
+    x_axis %in% colnames(test_3$data)
+  )
+
+  expect_true(
+    group %in% colnames(test_3$data)
+  )
+
+  expect_true(
+    facet %in% colnames(test_3$data)
+  )
+
+  # metric test 4 - first metric by rolling 4 weeks & group & facet -----------#
+  x_axis <- "roll_4week"
+  group <- dash_data$explore_group_facet[4]
+  facet <- dash_data$explore_group_facet[5]
+
+  test_4 <- dash_data$app_metrics(
+    metric_ids = mets$metric_id[1],
+    {{ sym(x_axis) }},
+    {{ sym(group) }},
+    {{ sym(facet) }}
+  )
+
+  expect_true(
+    x_axis %in% colnames(test_4$data)
+  )
+
+  expect_true(
+    group %in% colnames(test_4$data)
+  )
+
+  expect_true(
+    facet %in% colnames(test_4$data)
+  )
+
+  # metric test 5 - 2 metrics, group by metric & another facet ----------------#
+  x_axis <- "roll_4week"
+  facet <- dash_data$explore_group_facet[5]
+
+  test_5 <- dash_data$app_metrics(
+    metric_ids = mets$metric_id[1:2],
+    {{ sym(x_axis) }},
+    {{ sym(facet) }}
+  )
+
+  expect_equal(
+    sort(unique(test_5$data$metric_id)),
+    sort(mets$metric_id[1:2])
+  )
+
+  expect_true(
+    x_axis %in% colnames(test_5$data)
+  )
+
+  expect_true(
+    facet %in% colnames(test_5$data)
+  )
+
+  # metric 6 - first metric, edited dates -------------------------------------#
+  test_date_range <- c(dash_data$data_date_range[1] + 100,
+                       dash_data$data_date_range[2] - 100)
+
+  dash_data$date_range <- test_date_range
+
+  x_axis <- "date"
+
+  test_6 <- dash_data$app_metrics(
+    metric_ids = mets$metric_id[1],
+    {{ sym(x_axis) }}
+  )
+
+  expect_true(
+    x_axis %in% colnames(test_6$data)
+  )
+
+  expect_true(
+    min(test_6$data$date) == as.Date(test_date_range[1]) # assumes all dates in data
+  )
+
+  expect_true(
+    max(test_6$data$date) == test_date_range[2] # assumes all dates in data
+  )
 
 
+  # metric 7 - 2 metrics, edited dates, grouping variables --------------------#
+  test_date_range <- c(dash_data$data_date_range[1] + 50,
+                       dash_data$data_date_range[2] - 50)
 
+  dash_data$date_range <- test_date_range
 
+  x_axis <- "date"
+  facet <- dash_data$explore_group_facet[5]
+  group <- "calender_year"
+
+  test_7 <- dash_data$app_metrics(
+    metric_ids = mets$metric_id[1:2],
+    {{ sym(x_axis) }},
+    {{ sym(facet) }},
+    {{ sym(group) }}
+  )
+
+  expect_true(
+    x_axis %in% colnames(test_7$data)
+  )
+
+  expect_true(
+    min(test_7$data$date) == as.Date(test_date_range[1]) # assumes all dates in data
+  )
+
+  expect_true(
+    max(test_7$data$date) == test_date_range[2] # assumes all dates in data
+  )
 
 })
--- tests/testthat/test-utils_server.R
+++ tests/testthat/test-utils_server.R
@@ -5,10 +5,9 @@
 # create dash_data object
 dash_data <- app_data$new()
 
+dash_data$date_range <- dash_data$data_date_range
 
-
-
-# test remove_field ------------------------------------------------------------
+# test remove_field -----------------------------------------------------------#
 
 testthat::test_that("utils_server - remove_field", {
 
@@ -22,5 +21,236 @@ testthat::test_that("utils_server - remove_field", {
   expect_no_error(remove_field(df, z))
 
 
+
+})
+
+# explore_plot_details --------------------------------------------------------#
+
+testthat::test_that("utils_server - explore_plot_details", {
+
+  mets <- dash_data$metric_meta
+
+
+
+  # metric test 1 - first metric by date --------------------------------------#
+
+  test_input <- list()
+
+  test_input$metric_id <- dash_data$metric_meta$metric_id[1]
+  test_input$plot_group <- dash_data$explore_group_facet[1]
+  test_input$plot_facet <- dash_data$explore_group_facet[1]
+  test_input$plot_x_axis <- dash_data$explore_x_axis[1]
+  test_input$plot_y_axis <-  dash_data$explore_y_axis[1]
+
+  test_1 <- explore_plot_details(
+    ui_inputs = test_input,
+    mod_data = dash_data
+  )
+
+  expect_true(
+    test_1$details$metric_id == mets$metric_id[1]
+  )
+
+  expect_true(
+    test_1$details$metric_name ==
+      mets$metric_name[mets$metric_id == mets$metric_id[1]]
+  )
+
+  expect_true(
+    test_input$plot_x_axis %in% colnames(test_1$data)
+  )
+
+  expect_true(
+    max(test_1$data$date) == max(dash_data$stats19$date)
+  )
+
+  expect_equal(
+    sort(names(test_1)),
+    sort(c("data", "details", "plot_x_axis", "plot_y_axis", "x_name",
+           "y_name", "plot_group", "plot_facet", "title","subtitle"))
+  )
+
+  # metric test 2 - first metric by week start & grouping ---------------------#
+
+  test_input <- list()
+
+  test_input$metric_id <- dash_data$metric_meta$metric_id[1]
+  test_input$plot_group <- dash_data$explore_group_facet[3]
+  test_input$plot_facet <- dash_data$explore_group_facet[1]
+  test_input$plot_x_axis <- "week_start"
+  test_input$plot_y_axis <-  dash_data$explore_y_axis[1]
+
+  test_2 <- explore_plot_details(
+    ui_inputs = test_input,
+    mod_data = dash_data
+  )
+
+  expect_true(
+    test_2$details$metric_id == mets$metric_id[1]
+  )
+
+  expect_true(
+    test_2$details$metric_name ==
+      mets$metric_name[mets$metric_id == mets$metric_id[1]]
+  )
+
+  expect_true(
+    test_input$plot_x_axis %in% colnames(test_2$data)
+  )
+
+  expect_true(
+    test_input$plot_group %in% colnames(test_2$data)
+  )
+
+  # metric test 3 - first metric by rolling 3 months & group & facet ----------#
+
+  test_input <- list()
+
+  test_input$metric_id <- dash_data$metric_meta$metric_id[1]
+  test_input$plot_group <- dash_data$explore_group_facet[3]
+  test_input$plot_facet <- dash_data$explore_group_facet[4]
+  test_input$plot_x_axis <- "roll_3month"
+  test_input$plot_y_axis <-  dash_data$explore_y_axis[1]
+
+  test_3 <- explore_plot_details(
+    ui_inputs = test_input,
+    mod_data = dash_data
+  )
+
+  expect_true(
+    test_input$plot_x_axis  %in% colnames(test_3$data)
+  )
+
+  expect_true(
+    test_input$plot_group %in% colnames(test_3$data)
+  )
+
+  expect_true(
+    test_input$plot_facet %in% colnames(test_3$data)
+  )
+
+  # metric test 4 - first metric by rolling 4 weeks & group & facet -----------#
+
+  test_input <- list()
+
+  test_input$metric_id <- dash_data$metric_meta$metric_id[1]
+  test_input$plot_group <- dash_data$explore_group_facet[4]
+  test_input$plot_facet <- dash_data$explore_group_facet[5]
+  test_input$plot_x_axis <- "roll_4week"
+  test_input$plot_y_axis <-  dash_data$explore_y_axis[1]
+
+  test_4 <- explore_plot_details(
+    ui_inputs = test_input,
+    mod_data = dash_data
+  )
+
+  expect_true(
+    test_input$plot_x_axis %in% colnames(test_4$data)
+  )
+
+  expect_true(
+    test_input$plot_group %in% colnames(test_4$data)
+  )
+
+  expect_true(
+    test_input$plot_facet %in% colnames(test_4$data)
+  )
+
+
+
+  # metric test 5 - 2 metrics, group by metric & another facet ----------------#
+
+  test_input <- list()
+
+  test_input$metric_id <- dash_data$metric_meta$metric_id[1:2]
+  test_input$plot_group <- dash_data$explore_group_facet[1]
+  test_input$plot_facet <- dash_data$explore_group_facet[5]
+  test_input$plot_x_axis <- "roll_4week"
+  test_input$plot_y_axis <-  dash_data$explore_y_axis[1]
+
+  test_5 <- explore_plot_details(
+    ui_inputs = test_input,
+    mod_data = dash_data
+  )
+
+  expect_equal(
+    sort(unique(test_5$data$metric_id)),
+    sort(mets$metric_id[1:2])
+  )
+
+  expect_true(
+    test_input$plot_x_axis %in% colnames(test_5$data)
+  )
+
+  expect_true(
+    test_input$plot_facet %in% colnames(test_5$data)
+  )
+
+  # metric 6 - first metric, edited dates -------------------------------------#
+
+  test_date_range <- c(dash_data$data_date_range[1] + 100,
+                       dash_data$data_date_range[2] - 100)
+
+  dash_data$date_range <- test_date_range
+
+  test_input <- list()
+
+  test_input$metric_id <- dash_data$metric_meta$metric_id[1]
+  test_input$plot_group <- dash_data$explore_group_facet[1]
+  test_input$plot_facet <- dash_data$explore_group_facet[1]
+  test_input$plot_x_axis <- "date"
+  test_input$plot_y_axis <-  dash_data$explore_y_axis[1]
+
+  test_6 <- explore_plot_details(
+    ui_inputs = test_input,
+    mod_data = dash_data
+  )
+
+  expect_true(
+    test_input$plot_x_axis %in% colnames(test_6$data)
+  )
+
+  expect_true(
+    min(test_6$data$date) == as.Date(test_date_range[1]) # assumes all dates in data
+  )
+
+  expect_true(
+    max(test_6$data$date) == test_date_range[2] # assumes all dates in data
+  )
+
+
+  # metric 7 - 2 metrics, edited dates, grouping variables --------------------#
+
+  test_date_range <- c(dash_data$data_date_range[1] + 50,
+                       dash_data$data_date_range[2] - 50)
+
+  dash_data$date_range <- test_date_range
+
+
+  test_input <- list()
+
+  test_input$metric_id <- dash_data$metric_meta$metric_id[1]
+  test_input$plot_group <- "calender_year"
+  test_input$plot_facet <- dash_data$explore_group_facet[5]
+  test_input$plot_x_axis <- "date"
+  test_input$plot_y_axis <-  dash_data$explore_y_axis[1]
+
+  test_7 <- explore_plot_details(
+    ui_inputs = test_input,
+    mod_data = dash_data
+  )
+
+  expect_true(
+    test_input$plot_x_axis %in% colnames(test_7$data)
+  )
+
+  # x axis date but grouping by year, should have changed 'date' to 'day_month'
+  expect_false(
+    test_input$plot_x_axis %in% colnames(test_7$data)
+  )
+
+  expect_true(
+    "day_month" %in% colnames(test_7$data)
+  )
 
 })
